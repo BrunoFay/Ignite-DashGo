8407489c670d277e9a9774b024f60207810da9a1
bruno <brunofay1@hotmail.com>
2022-11-21 18:40:54 -0300

    NEW add login form

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
-import {theme} from '../styles/theme'
+import {roboto, theme} from '../styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+
+      <main className={roboto.className}>
       <Component {...pageProps} />
+      </main>
     </ChakraProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,51 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { Button, Flex, FormLabel, Input } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <h1>hello</h1>
+    <Flex as="main" w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        flexDirection="column"
+        bg="gray.800"
+        w={360}
+        p={8}
+        borderRadius={6}
+      >
+        <FormLabel>
+          E-mail
+          <Input
+            type="email"
+            borderRadius={2}
+            variant="flushed"
+            bg="gray.900"
+            focusBorderColor="orange.500"
+            size="lg"
+            px={2}
+          />
+        </FormLabel>
+        <FormLabel>
+          Password
+          <Input
+            type="password"
+            borderRadius={2}
+            variant="flushed"
+            bg="gray.900"
+            focusBorderColor="orange.500"
+            size="lg"
+            px={2}
+          />
+        </FormLabel>
+        <Button
+          mt={6}
+          colorScheme="orange"
+          variant="solid"
+          type="submit"
+          size="lg"
+        >
+          {' '}
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
