29881fc220ef1e0e3f62e2521c9c9d7c98d246d1
bruno <brunofay1@hotmail.com>
2022-11-22 13:58:55 -0300

    NEW start sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,15 @@
+import { Flex } from '@chakra-ui/react'
 import React from 'react'
 import Header from '../components/Header'
+import Sidebar from '../components/Sidebar'
 
 export default function Dashboard() {
-  return <Header />
+  return (
+    <Flex h="100vh" direction="column">
+      <Header />
+      <Flex maxW={1480} w="100%" my="6" mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
+  )
 }
