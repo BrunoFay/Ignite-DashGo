08b3a6b11df2e9ea84468c7eee1478c992039c2b
bruno <brunofay1@hotmail.com>
2022-11-23 17:10:32 -0300

    NEW add react hook form to project and start login logic to integrate to backend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,24 @@
 import { Button, Flex, FormLabel, Input } from '@chakra-ui/react'
+import { useForm, SubmitHandler } from 'react-hook-form'
 
 export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+  const handleLogin: SubmitHandler<{ email: string; password: string }> = (
+    value,
+    e,
+  ) => {
+    e?.preventDefault()
+    console.log(value)
+  }
   return (
     <Flex as="main" w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -10,6 +28,7 @@ export default function Home() {
         w={360}
         p={8}
         borderRadius={6}
+        onSubmit={handleSubmit(handleLogin)}
       >
         <FormLabel>
           E-mail
@@ -21,6 +40,7 @@ export default function Home() {
             focusBorderColor="orange.500"
             size="lg"
             px={2}
+            {...register('email')}
           />
         </FormLabel>
         <FormLabel>
@@ -33,6 +53,7 @@ export default function Home() {
             focusBorderColor="orange.500"
             size="lg"
             px={2}
+            {...register('password')}
           />
         </FormLabel>
         <Button
